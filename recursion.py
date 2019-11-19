18151d066060be0823cad06caef7dce35e1b3faa
KuaGai <32660710+KuaGai@users.noreply.github.com>
2019-11-18 21:33:12 -0500

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Week13
+
+
+def fibonacci(n):
+    #Implement the Fibonnaci Sequence
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return fibonacci(n-1) + fibonacci(n-2)
+
+def gcd(a, b):
+    #Implement Euclid’s GCD Algorithm
+    while b:
+        (a, b) = (b, (a % b))
+    return a
+
+    
+if __name__ == '__main__':
+    fibonacci(7)
+    gcd(18, 2)
